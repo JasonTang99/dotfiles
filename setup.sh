9e5cada5d598bab93245acc48137981292ffb3bf
JasonTang99 <1999jasontang@gmail.com>
2021-05-17 23:12:44 -0400

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+ln -s .gdbinit ~/.gdbinit
+ln -s .vimrc ~/.vimrc
+ln -s .tmux.conf ~/.tmux.conf
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
