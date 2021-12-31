4915d62015b20509324374d2a39d6d09141e6c62
Jason Tang <1999jasontang@gmail.com>
2021-12-30 23:02:17 -0500

    Fixed YCM issue

--- minimal_setup.sh
+++ minimal_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-ln -s .gdbinit ~/.gdbinit
-ln -s .vimrc ~/.vimrc
-ln -s .tmux.conf ~/.tmux.conf
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
